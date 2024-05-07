cb0476d7d1506f6546e88a58950e586303e95fe1
gabrieliglesias18 <gabriel.iglesias.freitas@gmail.com>
2024-05-07 10:32:59 -0300

    Criação exercicio_2.py

---
+++ entrada_saida/exercicio_2.py
@@ -0,0 +1,3 @@
+altura = int(input("Insira sua estatura em metros: "))
+conversao = estatura * 100
+print(f"A convensão de sua estatura em metros para centimetros são de: {conversao}" "centimetros")            
